a865423726d70e601352cfae5d7c58a26f303211
sangyeon <asylee02@gmail.com>
2023-11-07 15:49:52 +0900

    상세 페이지 제작

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import {format, register} from 'timeago.js';
+import {useNavigate} from 'react-router-dom'
 
 export default function VideoCard({ video }) {
-  return <div>{video.snippet.title}</div>;
+  const navigate = useNavigate();
+
+  const handleClick = function(){
+    navigate(`/videos/watch/${video.id}`, {state: {video: video}})
+  }
+  const {title, thumbnails,channelTitle, publishedAt} = video.snippet;
+  return <li>
+    <img onClick={handleClick} className='w-full hover:cursor-pointer' src={thumbnails.medium.url} alt ={title}/>
+    <div>
+      <p className='font-semibold my-2 line-clamp-2'>{title}</p>
+      <p className='text-sm opacity-80'>{channelTitle}</p>
+      <p className='text-sm opacity-80 '>{format(publishedAt)}</p>
+    </div>
+  </li>;
+
 }
